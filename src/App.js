7120ffc6c7a83390fa6321a85b8cc1d084fe92f6
Goutam Das <goutamdas676@gmail.com>
2021-12-30 15:33:21 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then(res => res.json())
+      .then(posts => {
+        setData(posts);
+        // grid.innerHTML = ''
+        // posts.forEach(post => {
+        //   const div = cardTemplate.content.cloneNode(true)
+        //   div.querySelector('[data-title]').textContent = post.title
+        //   div.querySelector('[data-body]').textContent = post.body
+        //   grid.append(div)
+        // })
+      })
+  }, [setData])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      {data?.length ? data?.map(post => <Card {...post} />) : <>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </>}
     </div>
   );
 }
 
 export default App;
+
+const Card = ({ title, body }) => (
+  <div class="card">
+    <div class="header">
+      <img class="header-img skeleton" src="https://source.unsplash.com/100x100/?nature" />
+      <div class="title" data-title>
+        {title ?? <>
+          <div class="skeleton skeleton-text"></div>
+          <div class="skeleton skeleton-text"></div>
+        </>}
+      </div>
+    </div>
+    <div data-body>
+      {body ?? <>
+        <div class="skeleton skeleton-text"></div>
+        <div class="skeleton skeleton-text"></div>
+        <div class="skeleton skeleton-text"></div>
+        <div class="skeleton skeleton-text"></div>
+      </>}
+    </div>
+  </div>
+)
